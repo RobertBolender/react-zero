34147a8825be2a7f8b8612fe2578c96eb6a4c23f
Robert Bolender <robert.bolender@faithlife.com>
2020-06-25 22:09:12 -0700

    Fix 404 page.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const html = htm.bind(React.createElement);
 
 const Route = {
   "/react-zero/": React.lazy(() => import("./routes/home/index.js")),
-  "/react-zero/*": React.lazy(() => import("./routes/lost/index.js")),
+  "*": React.lazy(() => import("./routes/lost/index.js")),
 };
 
 ReactDOM.render(
